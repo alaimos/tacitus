4a2264aee4f52ae6cb135f3b8a2aad5d93dab3b0
Salvatore Alaimo <alaimos@gmail.com>
2016-09-10 10:30:08 +0200

    Creating Task Panel and Fixes

--- www/tacitus/app/Utils/Permissions.php
+++ www/tacitus/app/Utils/Permissions.php
@@ -12,8 +12,6 @@ class Permissions
 
     const USER_PANELS = 'user-panels';
     const VIEW_DATASETS = 'view-datasets';
-    const VIEW_ALL_DATASETS = 'view-all-datasets';
-    const USE_ALL_DATASETS = 'use-all-datasets';
     const SUBMIT_DATASETS = 'submit-dataset';
     const DELETE_DATASETS = 'delete-datasets';
     const SELECT_FROM_DATASETS = 'select-from-datasets';
@@ -23,7 +21,19 @@ class Permissions
     const USE_TOOLS = 'use-tools';
     const INTEGRATE_DATASETS = 'integrate-datasets';
     const VIEW_JOBS = 'view-jobs';
-    const VIEW_ALL_JOBS = 'view-all-jobs';
     const ADMINISTER = 'administer-system';
+
+    /**
+     * @deprecated Check for Permissions::ADMINISTER instead
+     */
+    const VIEW_ALL_DATASETS = 'view-all-datasets';
+    /**
+     * @deprecated Check for Permissions::ADMINISTER instead
+     */
+    const USE_ALL_DATASETS = 'use-all-datasets';
+    /**
+     * @deprecated Check for Permissions::ADMINISTER instead
+     */
+    const VIEW_ALL_JOBS = 'view-all-jobs';
 
 }
